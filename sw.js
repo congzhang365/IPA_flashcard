e271d849ca0ff1371eac9766ae76715dcd11bfb4
congzhang365 <rolin365@gmail.com>
2026-09-22 22:38:40 +0000

    Deploying to gh-pages from @ congzhang365/IPA_flashcard@0cf348c6183d26bb58a792ae4ae22f2a5e89fd0c 🚀

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'ipa365-flashcards-v3';
+const APP_SHELL = ['./', './index.html', './manifest.json', './favicon.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => Promise.all(
+      cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).then((response) => {
+      if (response.ok && event.request.method === 'GET') {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      }
+      return response;
+    }).catch(() => caches.match(event.request))
+  );
+});
